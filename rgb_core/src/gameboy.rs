230ca8b119dfe21817773ec7eb763e09172195ac
joseph-wardle <joseph.m.wardle@gmail.com>
2025-07-08 12:26:38 +0300

    Added skeleton DMG container

---
+++ rgb_core/src/gameboy.rs
@@ -0,0 +1,56 @@
+use crate::apu::APU;
+use crate::cartridge::Cartridge;
+use crate::cpu::CPU;
+use crate::mmu::MMU;
+use crate::ppu::PPU;
+use crate::serial::Serial;
+
+pub struct DMG {
+    cpu: CPU,
+    ppu: PPU,
+    apu: APU,
+    pub bus: MMU,
+}
+
+impl DMG {
+    pub fn new(cartridge: Box<dyn Cartridge>) -> Self {
+        Self {
+            cpu: CPU::new(),
+            ppu: PPU::new(),
+            apu: APU::new(),
+            bus: MMU::new(cartridge),
+        }
+    }
+    
+    pub fn new_post_bios(cartridge: Box<dyn Cartridge>) -> Self {
+        Self {
+            cpu: CPU::new_post_bios(),
+            ppu: PPU::new(),
+            apu: APU::new(),
+            bus: MMU::new(cartridge),
+        }
+    }
+    
+    pub fn step_frame(&mut self) {
+        loop {
+            self.cpu.step(&mut self.bus);
+            // self.ppu.step(c, &mut self.bus);
+            // self.timer.step(c, &mut self.bus);
+            // self.apu.step(c);
+            // self.cpu.service_interrupts(&mut self.bus);
+            // cycles_this_frame += c;
+        }
+    }
+
+    pub fn run_until<F>(&mut self, mut condition: F, max_steps: usize)
+    where
+        F: FnMut(&Serial) -> bool,
+    {
+        for _ in 0..max_steps {
+            self.step_frame();
+            if condition(&self.bus.serial) {
+                break;
+            }
+        }
+    }
+}
